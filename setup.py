a33cf64f2ac5e89338d4991fc83351555efd0858
Jason Fried <me@jasonfried.info>
2015-02-18 19:55:12 -0800

    Update setup.py to set zip_safe=True
    You only turn this off if you have some c extensions or data files that are packaged with the module.

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     url='http://github.com/facebook/codemod',
     scripts=['src/codemod.py'],
     author='facebook',
-    description="""Codemod is a tool/library to assist you with large-scale codebase refactors that can be partially automated but still require human oversight and occasional intervention. Codemod was developed at Facebook and released as open source.""",
-    zip_safe=False,
+    description='Codemod is a tool/library to assist you with large-scale codebase refactors that can be partially automated but still require human oversight and occasional intervention. Codemod was developed at Facebook and released as open source.',
+    zip_safe=True
 )
